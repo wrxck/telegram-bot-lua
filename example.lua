5ff1d272157fe7b65b56b5c0fd4dc760cb63a980
Matt <wrxck0@gmail.com>
2017-03-05 12:37:04 +0000

    Fixed the example

--- example.lua
+++ example.lua
@@ -1,9 +1,10 @@
 --[[
     This example will echo messages back to the user who sent them, with an inline keyboard
-    which tells the user what their numerical ID is.
+    which tells the user the JSON for the callback_query.from object.
 ]]
 
 local api = require('telegram-bot-lua.core').configure('') -- Enter your token
+local json = require('dkjson')
 
 function api.on_message(message)
     if message.text then
@@ -26,8 +27,8 @@ end
 
 function api.on_callback_query(callback_query)
     api.answer_callback_query(
-        callback_query.id
-        'Your ID is: ' .. callback_query.from.id
+        callback_query.id,
+        json.encode(callback_query.from)
     )
 end
 
