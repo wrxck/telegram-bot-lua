d4c71672e981667c5fd3e31798b19c97f1b444c1
Matt <wrxck0@gmail.com>
2017-03-05 12:31:20 +0000

    Improved the example

--- example.lua
+++ example.lua
@@ -1,12 +1,34 @@
+--[[
+    This example will echo messages back to the user who sent them, with an inline keyboard
+    which tells the user what their numerical ID is.
+]]
+
 local api = require('telegram-bot-lua.core').configure('') -- Enter your token
 
 function api.on_message(message)
     if message.text then
         api.send_message(
             message,
-            message.text
+            message.text,
+            nil,
+            true,
+            false,
+            nil,
+            api.inline_keyboard():row(
+                api.row():callback_data_button(
+                    'Button',
+                    'callback_data'
+                )
+            )
         )
     end
+end
+
+function api.on_callback_query(callback_query)
+    api.answer_callback_query(
+        callback_query.id
+        'Your ID is: ' .. callback_query.from.id
+    )
 end
 
 api.run()
